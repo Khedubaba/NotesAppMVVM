957415a75963f5bcbf67cd26b1d4410d58bb562d
khedubaba <adityakhedekar49@gmail.com>
2020-05-05 12:46:48 +0530

    Swipe left or right to delete note functionality added.
    Signed-off-by: khedubaba <adityakhedekar49@gmail.com>

--- app/src/main/java/com/adityakhedekar/khedubaba/notesappmvvm/MainActivity.java
+++ app/src/main/java/com/adityakhedekar/khedubaba/notesappmvvm/MainActivity.java
@@ -1,14 +1,22 @@
 package com.adityakhedekar.khedubaba.notesappmvvm;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,6 +27,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     public static final int ADD_NOTE_REQUEST = 1;
     private NoteViewModel mNoteViewModel;
+
+    final NoteAdapter adapter = new NoteAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         //setHasFixedSize to true if you know that youu recyclerview layout(note_item) size and structure wont change this makes it more efficient
         recyclerView.setHasFixedSize(true);
 
-        final NoteAdapter adapter = new NoteAdapter();
         recyclerView.setAdapter(adapter);
 
         mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
@@ -50,6 +59,45 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(R.drawable.ic_warning)
+                        .setTitle("Delete note")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mNoteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+                                Toast.makeText(MainActivity.this, "Note deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mNoteViewModel = ViewModelProviders.of(MainActivity.this).get(NoteViewModel.class);
+                                mNoteViewModel.getAllNotes().observe(MainActivity.this, new Observer<List<Note>>() {
+                                    @Override
+                                    public void onChanged(List<Note> notes) {
+                                        //Update RecyclerView
+                                        adapter.setNotes(notes);
+                                    }
+                                });
+                                Toast.makeText(MainActivity.this, "Note deletion canceled", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+//                mNoteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+//                Toast.makeText(MainActivity.this, "Note deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
     }
 
     @Override
@@ -65,9 +113,55 @@ public class MainActivity extends AppCompatActivity {
             mNoteViewModel.insert(note);
 
             Toast.makeText(this, "Note is saved", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete_all_notes:
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(R.drawable.ic_warning)
+                        .setTitle("Delete note")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mNoteViewModel.deleteAllNotes();
+                                Toast.makeText(MainActivity.this, "All notes deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mNoteViewModel = ViewModelProviders.of(MainActivity.this).get(NoteViewModel.class);
+                                mNoteViewModel.getAllNotes().observe(MainActivity.this, new Observer<List<Note>>() {
+                                    @Override
+                                    public void onChanged(List<Note> notes) {
+                                        //Update RecyclerView
+                                        adapter.setNotes(notes);
+                                    }
+                                });
+                                Toast.makeText(MainActivity.this, "Note deletion canceled", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+
+//                mNoteViewModel.deleteAllNotes();
+//                Toast.makeText(this, "All notes deleted", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
